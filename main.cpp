e68649b6311ad0dd3c72fbe98bbbadd24b5c15a7
WangYunfeng <wangyunfeng@graphicchina.com>
2020-08-20 20:01:38 +0800

    更多立方体

--- main.cpp
+++ main.cpp
@@ -57,6 +57,21 @@ float vertices[] =
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+//10个立方体的位置
+glm::vec3 cubePositions[] =
+{
+  glm::vec3(0.0f,  0.0f,  0.0f),
+  glm::vec3(2.0f,  5.0f, -15.0f),
+  glm::vec3(-1.5f, -2.2f, -2.5f),
+  glm::vec3(-3.8f, -2.0f, -12.3f),
+  glm::vec3(2.4f, -0.4f, -3.5f),
+  glm::vec3(-1.7f,  3.0f, -7.5f),
+  glm::vec3(1.3f, -2.0f, -2.5f),
+  glm::vec3(1.5f,  2.0f, -2.5f),
+  glm::vec3(1.5f,  0.2f, -1.5f),
+  glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 //索引，用于索引绘制(Indexed Drawing)
 unsigned int indices[] =
 {
@@ -189,10 +204,6 @@ int main()
 	ourShader.use();
 	ourShader.setInt("ourTexture1", 0);
 	ourShader.setInt("ourTexture2", 1);
-
-	//模型矩阵
-	glm::mat4 model(1.0f);
-	//model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	
 	//观察矩阵
 	glm::mat4 view(1.0f);
@@ -202,8 +213,8 @@ int main()
 	glm::mat4 projection;
 	projection = glm::perspective(glm::radians(45.0f), 1.0f * 800 / 600, 0.1f, 100.0f);
 
+	//变换矩阵
 	glm::mat4 trans(1.0f);
-	/*trans = projection * view * model;*/
 
 	//启用深度测试
 	glEnable(GL_DEPTH_TEST);
@@ -217,14 +228,7 @@ int main()
 		//在循环开始时清屏
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//设置清屏颜色【状态设置】
 		glClear(GL_COLOR_BUFFER_BIT);//清空颜色缓冲，整个颜色缓冲填充为清屏颜色【状态使用】
-		glClear(GL_DEPTH_BUFFER_BIT);
-
-		//旋转
-		model = glm::rotate(model, /*(float)glfwGetTime()**/glm::radians(1.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		trans = projection * view * model;
-
-		int transLoc = glGetUniformLocation(ourShader.ID, "trans");
-		glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(trans));
+		glClear(GL_DEPTH_BUFFER_BIT);//清空深度缓冲
 
 		//渲染指令
 		//......
@@ -243,7 +247,21 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			//模型矩阵
+			glm::mat4 model(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));//不是正在旋转，都是旋转过的，角度不同
+			
+			trans = projection * view * model;
+
+			int transLoc = glGetUniformLocation(ourShader.ID, "trans");
+			glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(trans));
+			
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//线框模式(Wireframe Mode)，para1 指应用到所有三角形正、背面
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
